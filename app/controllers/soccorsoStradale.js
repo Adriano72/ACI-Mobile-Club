c7eb3acefbbb0aeebe6b34ff7090661b186986cf
balanza <ema.decu@gmail.com>
2015-01-16 16:23:53 +0100

    Selettore posizione mappa

--- app/controllers/soccorsoStradale.js
+++ app/controllers/soccorsoStradale.js
@@ -1,40 +1,54 @@
+var locationServices = require('locationServices');
+
 var args = arguments[0] || {};
 
 if (OS_ANDROID) {
-	var abx = require('com.alcoapps.actionbarextras');
+    var abx = require('com.alcoapps.actionbarextras');
 };
 
 function doopen(evt) {
-	if (OS_ANDROID) {
+    if (OS_ANDROID) {
 
-		init1();
+        init1();
 
-	} else {
-		//$.windowtitle.text = winTitle;
-	}
-	
-	//updateScreen();
+    } else {
+        //$.windowtitle.text = winTitle;
+    }
+
+    //updateScreen();
 }
 
 function init1() {
-	abx.displayHomeAsUp = true;
-	abx.title = "Soccorso Stradale";
-	abx.titleFont = "ACI Type Regular.otf";
-	abx.titleColor = "#003772";
-	_.defer(init2);
+    abx.displayHomeAsUp = true;
+    abx.title = "Soccorso Stradale";
+    abx.titleFont = "ACI Type Regular.otf";
+    abx.titleColor = "#003772";
+    _.defer(init2);
 }
 
 function init2() {
-	$.win.activity.invalidateOptionsMenu();
+    $.win.activity.invalidateOptionsMenu();
 }
 
-function chiamaSoccorso(){
-	Titanium.Platform.openURL('tel:803116');
+function chiamaSoccorso() {
+    Titanium.Platform.openURL('tel:803116');
 }
 
 
-$.tipoAiuto.addEventListener('change', function(e){
-	console.log('change', e);
+$.tipoAiuto.addEventListener('change', function(e) {
+    console.log('change', e);
 });
 
 
+
+$.mapview.addEventListener('complete', function() {
+    //la posizione iniziale è quella dell'utente
+    locationServices.getUserLocation(function(coo) {
+        $.mapview.setLocation(coo);
+    });
+});
+
+
+$.mapview.addEventListener('regionchanged', function(e){
+	console.log('centro mappa: ', $.mapview.getRegion());
+});
