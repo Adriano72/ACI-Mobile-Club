cda7a8a6ae2e7d35baad7e7d450ff2780fc189f5
balanza <ema.decu@gmail.com>
2015-01-27 17:21:30 +0100

    aggiunta possibilità di specificare header

--- app/lib/rest.js
+++ app/lib/rest.js
@@ -10,48 +10,66 @@
  * @param  {string}   method     metodo http della chiama
  * @param  {object}   parameters [parametri da allegare alla chiamata]
  * @param  {Function} callback   callback di risposta nel formato function(err, result)
+ * @param  {object}   headers    eventuali intestazioni della richiesta http
  */
-function call(url, method, parameters, callback) {
+function call(url, method, parameters, callback, headers) {
     Ti.API.debug("webServiceCall " + url);
 
-    if (!Titanium.Network.online) {
+    /*if (!Titanium.Network.online) {
         //alert(L("noconnectionapp"));
         if (callback) {
             callback('noconnection');
         }
         return;
-    }
+    }*/
 
 
 
     var xhr = Titanium.Network.createHTTPClient();
 
-    if (url.substr(0, 5) === "https") {
+    /*if (url.substr(0, 5) === "https") {
         xhr.validatesSecureCertificate = false;
         xhr.tlsVersion = Ti.Network.TLS_VERSION_1_0;
+    }*/
+
+    //controllo che la url non abbia dichiarato delle credenziali
+    //in caso, devo aggiungere l'header di autenticazione
+    /*    var U = exports.parseUrl(url);
+    if (U.username && U.password) {
+        console.log("aggiunge autorizzazione");
+        xhr.setRequestHeader('Authorization', exports.formatBasicAuthentication(U.username, U.password));
+    } else {
+        console.log("NON aggiunge autorizzazione");
+
     }
-    /*
-	 xhr.onsendstream = function(e) {
+    */
 
-	 if (windowEnabled) {
-	 var progress = (OS_ANDROID) ? e.progress : e.progress;
-	 if (Math.round(progress * 100) <= 100) {
-	 //activityIndicator.message = L("loading") + " " + (Math.round(progress * 100)).toString().replace(".", "") + '%';
-	 }
-	 }
 
-	 };
-	 */
+
+
+
+    /**
+     * Definisco il gestore dell'errore
+     * @param  {[type]} e [description]
+     * @return {[type]}   [description]
+     */
     xhr.onerror = function(e) {
         Ti.API.debug("xhr onError " + JSON.stringify(e));
 
 
-
+        Ti.API.error("ERRORE RISPOSTA SERVER: " + this.message);
         if (callback) {
+            e.message = this.message;
             callback(e);
         }
     };
 
+
+    /**
+     * Definisco il gestore per il caricamento della risposta
+     * @param  {[type]} e [description]
+     * @return {[type]}   [description]
+     */
     xhr.onload = function(e) {
         Ti.API.debug("xhr onSuccess");
 
@@ -81,22 +99,75 @@ function call(url, method, parameters, callback) {
 
     };
 
+    var uname = 'acimobileclub';
+    var pass = 'Iniziale$01';
+    authstr = 'Basic ' + Titanium.Utils.base64encode(uname + ':' + pass);
+
+
+
     Ti.API.debug(url);
     Ti.API.debug(parameters);
+
+    //formatta url e parametri a seconda del tipo di richiesta
     if (method.toUpperCase() == "GET") {
         url = url + '?' + toQueryString(parameters);
-        Ti.API.debug(url);
-        xhr.open(method, url);
-        xhr.send();
+        parameters = undefined;
     } else {
-        xhr.open(method, url);
-        xhr.send(JSON.stringify(parameters));
-
+        parameters = JSON.stringify(parameters);
     }
 
 
+    xhr.open(method, url);
+
+    //aggiungo gli eventuali header
+    //bisogna farlo tra open() e send()!
+    // https://twitter.com/balanza/status/560108493945192449
+    for (var k in headers) {
+        if (headers.hasOwnProperty(k)) {
+            var h = headers[k];
+            console.log("set header", k, h);
+            xhr.setRequestHeader(k, h);
+        }
+    }
+
+    xhr.send(parameters);
 
 
+
+
+};
+
+/**
+ * Utility che formatta una coppia username/password in un token valido per l'autenticazione http basic
+ * http://it.wikipedia.org/wiki/Basic_access_authentication#Lato_Client
+ * @param  {string} username
+ * @param  {string} password
+ * @return {string}          token formattato secondo le specifiche
+ */
+exports.formatBasicAuthentication = function(username, password) {
+    return 'Basic ' + Ti.Utils.base64encode(username + ':' + password);
+}
+
+/**
+ * Utility per il parsing della url
+ * playground: http://jsbin.com/rijiha/1/edit?html,js,console
+ * @param  {string} url url da esaminare
+ * @return {object}     hash con le parti della url
+ */
+exports.parseUrl = function(url) {
+    var r = /^((http[s]?|ftp):\/)?\/?(([\w\-\.]+):([\w\-\.\$]+)@)?([^:\/\s]+)((\/\w+)*?(:([0-9]+))\/)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?$/;
+    var tmp = r.exec(url);
+
+    return {
+        original: tmp[0],
+        protocol: tmp[2],
+        username: tmp[4],
+        password: tmp[5],
+        host: tmp[6],
+        port: tmp[10],
+        path: tmp[11],
+        query: tmp[12]
+    };
 };
 
 function toQueryString(obj) {
@@ -113,9 +184,10 @@ function toQueryString(obj) {
  * @param  {string}   url        url a cui indirizzare la chiamata
  * @param  {object}   parameters [parametri da allegare alla chiamata]
  * @param  {Function} callback   callback di risposta nel formato function(err, result)
+ * @param  {object}   headers    eventuali intestazioni della richiesta http
  */
-exports.get = function(url, parameters, callback) {
-    call(url, 'GET', parameters, callback);
+exports.get = function(url, parameters, callback, headers) {
+    call(url, 'GET', parameters, callback, headers);
 };
 
 /**
@@ -123,9 +195,10 @@ exports.get = function(url, parameters, callback) {
  * @param  {string}   url        url a cui indirizzare la chiamata
  * @param  {object}   parameters [parametri da allegare alla chiamata]
  * @param  {Function} callback   callback di risposta nel formato function(err, result)
+ * @param  {object}   headers    eventuali intestazioni della richiesta http
  */
-exports.post = function(url, parameters, callback) {
-    call(url, 'POST', parameters, callback);
+exports.post = function(url, parameters, callback, headers) {
+    call(url, 'POST', parameters, callback, headers);
 };
 
 /**
@@ -133,7 +206,8 @@ exports.post = function(url, parameters, callback) {
  * @param  {string}   url        url a cui indirizzare la chiamata
  * @param  {object}   parameters [parametri da allegare alla chiamata]
  * @param  {Function} callback   callback di risposta nel formato function(err, result)
+ * @param  {object}   headers    eventuali intestazioni della richiesta http
  */
-exports.put = function(url, parameters, callback) {
-    call(url, 'PUT', parameters, callback);
+exports.put = function(url, parameters, callback, headers) {
+    call(url, 'PUT', parameters, callback, headers);
 };
