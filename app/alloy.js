66b7a983b97902eb09ac4638ee11be1a8dd072b7
balanza <ema.decu@gmail.com>
2015-09-15 16:13:07 +0200

    implementazione prototipo su iOS e android

--- app/alloy.js
+++ app/alloy.js
@@ -128,8 +128,14 @@ _.defer(require('user').refreshData);
 
 
 if (Alloy.CFG.SysReport_Enabled && Alloy.CFG.SysReport_UseShake) {
-   require('sysReportCommon').enableShake();
+    require('sysReportCommon').enableShake();
 }
 
-
-
+var push = require('push');
+push.retrieveDeviceToken(function(err, res) {
+    if(err){
+        console.error(err);
+    } else {
+        push.subscribeToChannel('news_alerts');
+    }
+});
--- app/lib/push.js
+++ app/lib/push.js
@@ -1,45 +1,142 @@
-
 /**
- * Funzioni locali
+ * Modulo per la gestione delle push notification
+ * generalizza il comportamento tra le piattaforme e tiene traccia dei token
  */
 
+var deviceToken;
+
+var Cloud = require("ti.cloud");
 
 /**
  * Callback da chiamare quando ri riceve una push notification
  * @param  {[type]} e [description]
  */
-function onNotification(e){}
+function onNotification(e) {
+    console.log('retrieveDeviceToken, onNotification', e);
+
+    if (OS_IOS) {
+        var badge = Math.max(0, Titanium.UI.iPhone.getAppBadge() - 1);
+        Titanium.UI.iPhone.setAppBadge(badge);
+
+        Cloud.PushNotifications.setBadge({
+            device_token: deviceToken,
+            badge_number: badge
+        }, function(r, p) {
+            console.log('setBadge', r, p);
+        });
+    }
+}
 
 
 /**
  * Metodo cross-platorm per abilitare il device a ricevere push notification
  * @return {[type]} [description]
  */
-function retrieveDeviceToken(){
+function retrieveDeviceToken(cb) {
 
-	function onSuccess(e){
+    var types = OS_IOS ? [
+        Ti.App.iOS.USER_NOTIFICATION_TYPE_ALERT,
+        Ti.App.iOS.USER_NOTIFICATION_TYPE_SOUND,
+        Ti.App.iOS.USER_NOTIFICATION_TYPE_BADGE
+    ] : undefined;
 
-	}
 
-	function onError(e){
+    function onSuccess(e) {
+        console.log('retrieveDeviceToken, success', e);
+        deviceToken = e.deviceToken;
+        cb && cb(null, e);
+    }
 
-	}
+    function onError(e) {
+        console.log('retrieveDeviceToken, error', e);
+        cb && cb(e);
 
-	if(OS_IOS){
+    }
 
-		Ti.Network.registerForPushNotifications({
-			success: onSuccess,
-			error: onError,
-			callback: onNotification
-		});
+    function requestToken() {
+        console.log('requestToken');
+        if (OS_IOS) {
 
-	} else if(OS_ANDROID) {
+            Ti.Network.registerForPushNotifications({
+                success: onSuccess,
+                error: onError,
+                types: types,
+                callback: onNotification
+            });
 
-	} else {
-		//non supportiamo altre piattaforme al momento
-		onError('piattaforma non supportata');
-	}
+        } else if (OS_ANDROID) {
 
+            var cp = require('ti.cloudpush');
+
+            cp.retrieveDeviceToken({
+                success: onSuccess,
+                error: onError
+            });
+
+            cp.addEventListener('callback', onNotification);
+
+        } else {
+            //non supportiamo altre piattaforme al momento
+            onError('piattaforma non supportata');
+        }
+    }
+
+
+    if (OS_IOS && parseInt(Ti.Platform.version.split(".")[0]) >= 8) {
+        // Wait for user settings to be registered before registering for push notifications
+        Ti.App.iOS.addEventListener('usernotificationsettings', function registerForPush() {
+            console.log('requestToken usernotificationsettings');
+
+            // Remove event listener once registered for push notifications
+            Ti.App.iOS.removeEventListener('usernotificationsettings', registerForPush);
+
+            requestToken();
+        });
+
+        console.log('requestToken registerUserNotificationSettings');
+
+        // Register notification types to use
+        Ti.App.iOS.registerUserNotificationSettings({
+            types: types
+        });
+    } else {
+        requestToken();
+    }
+
+
+}
+
+
+
+
+function subscribeToChannel(channel) {
+    // Subscribes the device to the 'news_alerts' channel
+    // Specify the push type as either 'android' for Android or 'ios' for iOS
+    Cloud.PushNotifications.subscribeToken({
+        device_token: deviceToken,
+        channel: channel,
+        type: Ti.Platform.name == 'android' ? 'android' : 'ios'
+    }, function(e) {
+        if (e.success) {
+            alert('Subscribed');
+        } else {
+            alert('Error:\n' + ((e.error && e.message) || JSON.stringify(e)));
+        }
+    });
+}
+
+function unsubscribeToChannel(channel) {
+    // Unsubscribes the device from the 'test' channel
+    Cloud.PushNotifications.unsubscribeToken({
+        device_token: deviceToken,
+        channel: channel,
+    }, function(e) {
+        if (e.success) {
+            alert('Unsubscribed');
+        } else {
+            alert('Error:\n' + ((e.error && e.message) || JSON.stringify(e)));
+        }
+    });
 }
 
 
@@ -47,7 +144,9 @@ function retrieveDeviceToken(){
  * Public API
  */
 
-
-
-
-exports.retrieveDeviceToken = function(){}
+/**
+ * Metodo cross-platorm per abilitare il device a ricevere push notification
+ */
+exports.retrieveDeviceToken = retrieveDeviceToken;
+exports.subscribeToChannel = subscribeToChannel;
+exports.unsubscribeToChannel = unsubscribeToChannel;
